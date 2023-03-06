aace64b4c3ac6886ce67f398a6130eb44d978069
yerimechoi <yerimechoi@gmail.com>
2023-03-06 01:55:54 -0500

    Deleted reaction model since it's a subdocument schema in the Thought model.

--- models/Thought.js
+++ models/Thought.js
@@ -1,6 +1,34 @@
-const { Schema, model } = require('mongoose');
-const reactionSchema = require('./Reaction');
+const { Schema, model, Types } = require('mongoose');
 const moment = require('moment');
+
+const reactionSchema = new Schema(
+    {
+        reactionId: {
+            type: Schema.Types.ObjectId,
+            default: () => new Types.ObjectId()
+        },
+        reactionBody: {
+            type: String,
+            required: true,
+            maxlength: 280
+        },
+        username: {
+            type: String,
+            required: true
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: (createdAtVal) => moment(createdAtVal).format('MMM DD, YYYY [at] hh:mm a')
+        }
+    },
+    {
+        toJSON: {
+            getters: true,
+        },
+        id: false
+    }
+);
 
 const thoughtSchema = new Schema(
     {
